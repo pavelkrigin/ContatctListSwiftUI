346f50f8448f557be2857f5046a02da3267fd4dc
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-11-02 21:37:18 +0300

    Initial Commit

---
+++ ContatctListSwiftUI/ContatctListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContatctListSwiftUIApp.swift
+//  ContatctListSwiftUI
+//
+//  Created by Pavel Krigin on 2.11.22..
+//
+
+import SwiftUI
+
+@main
+struct ContatctListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
